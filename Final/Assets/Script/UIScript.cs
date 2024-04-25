c87e4dc0173748c73547ff81928fb56a1534a004
TheSquish18 <mp5896@nyu.edu>
2024-04-25 16:18:18 -0400

    tried to fix numbers

--- Final/Assets/Script/UIScript.cs
+++ Final/Assets/Script/UIScript.cs
@@ -12,6 +12,7 @@ public class UIScript : MonoBehaviour
     GameObject speedLine;
     GameObject speedText;
     float maxVel = Mathf.Sqrt(3); //max velocity for velocity input
+    public float numberDisplayTime = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +21,29 @@ public class UIScript : MonoBehaviour
         playerScript = player.GetComponent<playerCupcake>();
         speedLine = GameObject.Find("SpeedLine");
         speedText = GameObject.Find("SpeedText");
+        StartCoroutine(numbersLessFast());
     }
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    //float curSpeed = Mathf.Abs(playerScript.velocityInput.magnitude*30);
+    //    //float transparency = curSpeed / maxVel;
+    //    //speedLine.GetComponent<RawImage>().color = new Color(255, 255, 255, transparency);
+    //    //speedText.GetComponent<TextMeshProUGUI>().text = "Speed: " 
+    //    //        + (curSpeed * playerScript.playerSpeed * Time.deltaTime ).ToString();
+    //    //speedText.GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255, transparency);
+    //}
+
+    IEnumerator numbersLessFast()
     {
-        float curSpeed = Mathf.Abs(playerScript.velocityInput.magnitude);
+        yield return new WaitForSeconds(numberDisplayTime);
+        float curSpeed = Mathf.Abs(playerScript.velocityInput.magnitude * 30);
         float transparency = curSpeed / maxVel;
         speedLine.GetComponent<RawImage>().color = new Color(255, 255, 255, transparency);
-        speedText.GetComponent<TextMeshProUGUI>().text = "Speed: " 
-                + (curSpeed * playerScript.playerSpeed * Time.deltaTime ).ToString();
+        speedText.GetComponent<TextMeshProUGUI>().text = "Speed: "
+                + (curSpeed * playerScript.playerSpeed * Time.deltaTime).ToString();
         speedText.GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255, transparency);
+        StartCoroutine(numbersLessFast());
     }
 }
